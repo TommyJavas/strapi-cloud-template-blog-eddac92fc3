4193ea78f0bc298963fec07fbef98da0aa4e0edb
TommyJavas <310234576@qq.com>
2026-06-27 23:09:58 +0800

    fix: add non-www domain to CORS white list

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,17 +2,19 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  // { 替换开始 }
   {
     name: 'strapi::cors',
     config: {
-      origin: ['https://www.duck666.fun'], // 允许你的前端域名访问
+      // 💡 在这里把带 www 和不带 www 的域名都加上
+      origin: [
+        'https://www.duck666.fun', 
+        'https://duck666.fun'
+      ], 
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       keepHeaderOnError: true,
     },
   },
-  // { 替换结束 }
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
