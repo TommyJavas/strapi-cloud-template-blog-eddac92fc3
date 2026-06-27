d677cadbf780e5baa7350c7ee7c43eb175a697da
TommyJavas <310234576@qq.com>
2026-06-27 22:58:38 +0800

    fix: update CORS config for frontend domain

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,17 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  // { 替换开始 }
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://www.duck666.fun'], // 允许你的前端域名访问
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
+  // { 替换结束 }
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
